5712778c8b356f4db1b949a854e48b531288cb4b
himajakbk <124979480+himajakbk@users.noreply.github.com>
2023-08-05 15:25:03 +0530

    Update variables.tf

--- networking/variables.tf
+++ networking/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS Region"
   type        = string
-  #default     = "us-east-1"
+  #default     = "ap-south-1"
 }
 
 
@@ -15,19 +15,19 @@ variable "vpc_name" {
 variable "vpc_cidr_block" {
   description = "VPC CIDR Block"
   type        = string
-  #default     = "10.0.0.0/16"
+  #default     = "10.14.0.0/16"
 }
 
 # VPC Availability Zones
 variable "vpc_availability_zones" {
   description = "VPC Availability Zones"
   type        = list(string)
-  #default     = ["us-east-1a", "us-east-1b"]
+  #default     = ["ap-south-1a", "ap-south-1b"]
 }
 
 # VPC Public Subnets
 variable "vpc_public_subnets" {
   description = "VPC Public Subnets"
   type        = list(string)
-  #default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  #default     = ["10.14.101.0/24", "10.14.102.0/24"]
 }
