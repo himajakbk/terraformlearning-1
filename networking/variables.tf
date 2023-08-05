bdfa340ce05cc32dba4cdbe25bf73e613b130a50
himajakbk <124979480+himajakbk@users.noreply.github.com>
2023-08-05 15:56:42 +0530

    Update variables.tf

--- networking/variables.tf
+++ networking/variables.tf
@@ -15,7 +15,7 @@ variable "vpc_name" {
 variable "vpc_cidr_block" {
   description = "VPC CIDR Block"
   type        = string
-  #default     = "10.0.0.0/16"
+  #default     = "172.31.0.0/16"
 }
 
 # VPC Availability Zones
