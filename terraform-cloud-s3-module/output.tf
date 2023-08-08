a4a2b4d010d416f41bb12c2b526c64cba5d914ce
himajakbk <124979480+himajakbk@users.noreply.github.com>
2023-08-08 12:36:39 +0530

    Update output.tf

--- terraform-cloud-s3-module/output.tf
+++ terraform-cloud-s3-module/output.tf
@@ -1,19 +1,19 @@
 output "arn" {
   description = "ARN OF BUCKET"
-  value       = aws_s3_bucket.aws_s3_bucket_terraform_cloud.arn
+  value       = aws_s3_bucket_terraform_cloud.arn
 }
 
 output "name" {
   description = "NAME OF BUCKET"
-  value       = aws_s3_bucket.aws_s3_bucket_terraform_cloud.id
+  value       = aws_s3_bucket_terraform_cloud.id
 }
 
 output "domain" {
   description = "MAIN DOMAIN OF BUCKET"
-  value       = aws_s3_bucket.aws_s3_bucket_terraform_cloud.website_domain
+  value       = aws_s3_bucket_terraform_cloud.website_domain
 }
 
 output "endpoint" {
   description = "URL OF BUCKET"
-  value       = aws_s3_bucket.aws_s3_bucket_terraform_cloud.website_endpoint
+  value       = aws_s3_bucket_terraform_cloud.website_endpoint
 }
