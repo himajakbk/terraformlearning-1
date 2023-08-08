5772eb57ca1a79458449f5299ebb0675b9277889
himajakbk <124979480+himajakbk@users.noreply.github.com>
2023-08-08 11:04:16 +0530

    Update s3.tf

--- s3-using-private-module/s3.tf
+++ s3-using-private-module/s3.tf
@@ -1,5 +1,5 @@
-module "bucket_s3_terraform_cloud" {
-  source  = "app.terraform.io/himajakbkorganization/s3-module/cloud"
+module "s3module" {
+  source  = "app.terraform.io/himajakbkorganization/s3module/cloud"
   version = "1.0.0"
   name_bucket = var.s3_bucket_name
   tags_bucket = var.s3_bucket_tag
