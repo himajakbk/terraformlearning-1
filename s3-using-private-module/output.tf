d2a78d4de481b2627bed7ed09397e47dcf05da18
himajakbk <124979480+himajakbk@users.noreply.github.com>
2023-08-08 12:34:58 +0530

    Update output.tf

--- s3-using-private-module/output.tf
+++ s3-using-private-module/output.tf
@@ -3,23 +3,23 @@
 #ARN
 output "bucket_arn" {
   description = "Bucket ARN"
-  value = module.aws_s3_bucket.aws_s3_bucket_terraform_cloud.arn 
+  value = module.aws_s3_bucket_terraform_cloud.arn 
 }
 
 #Bucket Name
 output "bucket_name" {
   description = "Bucket Name"
-  value = module.aws_s3_bucket.aws_s3_bucket_terraform_cloud.name
+  value = module.aws_s3_bucket_terraform_cloud.name
 }
 
 #Domain
 output "bucket_domain" {
   description = "domain_of_bucket"
-  value = module.aws_s3_bucket.aws_s3_bucket_terraform_cloud.domain
+  value = module.aws_s3_bucket_terraform_cloud.domain
 }
 
 #Bucket URL
 output "Bucket_URL" {
   description = "URL OF BUCKET"
-  value = module.aws_s3_bucket.aws_s3_bucket_terraform_cloud.endpoint
+  value = module.aws_s3_bucket_terraform_cloud.endpoint
 }
